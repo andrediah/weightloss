6e00f38230e16dfd21929276530cd24465819149
OPTIMUS\Andre <andre@diahdom.net>
2026-04-25 18:44:29 -0400

    feat(bp): add EF migration for Category column on BloodPressureEntries

---
+++ WeightLossTracker/Migrations/20260425224417_AddBpCategory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeightLossTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBpCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "BloodPressureEntries",
+                type: "TEXT",
+                maxLength: 20,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "BloodPressureEntries");
+        }
+    }
+}
